3c47a8be0635c666e23d26d67f6f085a6de67d73
ivanturnovski <ivanturnovski@hotmail.com>
2022-02-28 23:45:25 +0100

    Add flickity carousel

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,28 @@
-// Put your application javascript here
+function carousel() {
+	return {
+	  active: 0,
+	  init() {
+		var flkty = new Flickity(this.$refs.carousel, {			
+			autoPlay: 2500, 		
+		  	wrapAround: true
+		});
+		flkty.on('change', i => this.active = i);
+	  }
+	}
+  }
+  
+  function carouselFilter() {
+	return {
+	  active: 0,
+	  changeActive(i) {
+		this.active = i;
+		
+		this.$nextTick(() => {
+		  let flkty = Flickity.data( this.$el.querySelectorAll('.carousel')[i] );
+		  flkty.resize();
+		  
+		  console.log(flkty);
+		});
+	  }
+	}
+  }
