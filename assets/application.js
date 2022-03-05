4335a2e812b31b0339c0f4019be43839f505f1ff
ivanturnovski <ivanturnovski@hotmail.com>
2022-03-05 02:50:52 +0100

    Cart drawer update without page reload

--- assets/application.js
+++ assets/application.js
@@ -1,63 +1,61 @@
 function carousel() {
 	return {
-	  active: 0,
-	  init() {
-		var flkty = new Flickity(this.$refs.carousel, {			
-			autoPlay: 2500, 		
-		  	wrapAround: true
-		});
-		flkty.on('change', i => this.active = i);
-	  }
-	}
-  }
-  
-  function carouselFilter() {
+		active: 0,
+		init() {
+			var flkty = new Flickity(this.$refs.carousel, {
+				autoPlay: 2500,
+				wrapAround: true,
+			});
+			flkty.on('change', (i) => (this.active = i));
+		},
+	};
+}
+
+function carouselFilter() {
 	return {
-	  active: 0,
-	  changeActive(i) {
-		this.active = i;
-		
-		this.$nextTick(() => {
-		  let flkty = Flickity.data( this.$el.querySelectorAll('.carousel')[i] );
-		  flkty.resize();
-		  
-		  console.log(flkty);
-		});
-	  }
-	}
-  }
+		active: 0,
+		changeActive(i) {
+			this.active = i;
 
-  function showDrawer() {
-	  return  console.log('added to cart!'); 
-  }
+			this.$nextTick(() => {
+				let flkty = Flickity.data(this.$el.querySelectorAll('.carousel')[i]);
+				flkty.resize();
 
-  function myComponentAddToCart() {
-	return {		
-		AddToCart(refs) {  			           
+				console.log(flkty);
+			});
+		},
+	};
+}
+
+function myComponentAddToCart() {
+	return {
+		AddToCart(refs) {
 			console.log(refs.variant_id.value);
 			let formData = {
-				'items': [
-				{
-					'id': refs.variant_id.value,
-					'quantity': 1
-				}]
+				items: [
+					{
+						id: refs.variant_id.value,
+						quantity: 1,
+					},
+				],
 			};
 			fetch('/cart/add.js', {
 				method: 'POST',
 				headers: {
-				  'Content-Type': 'application/json'
+					'Content-Type': 'application/json',
 				},
-				body: JSON.stringify(formData)
-			  })
-			  .then(response => {
-				return response.json();
-			  })
-			  .then(response => {
-				showDrawer();
-			  })
-			  .catch((error) => {
-				console.error('Error:', error);
-			  });
-		}
-	}
+				body: JSON.stringify(formData),
+			})
+				.then((response) => {
+					return response.json();
+				})
+				.then((response) => {
+					this.$dispatch('cart-updated');
+					this.$dispatch('toggle-cart');
+				})
+				.catch((error) => {
+					console.error('Error:', error);
+				});
+		},
+	};
 }
