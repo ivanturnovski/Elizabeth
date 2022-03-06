488955bd94a299f411f9236d5b28a44739f708c2
ivanturnovski <ivanturnovski@hotmail.com>
2022-03-06 13:39:23 +0100

    more improvements carousel

--- assets/application.js
+++ assets/application.js
@@ -1,32 +1,3 @@
-function carousel() {
-	return {
-		active: 0,
-		init() {
-			var flkty = new Flickity(this.$refs.carousel, {
-				autoPlay: 2500,
-				wrapAround: true,
-			});
-			flkty.on('change', (i) => (this.active = i));
-		},
-	};
-}
-
-function carouselFilter() {
-	return {
-		active: 0,
-		changeActive(i) {
-			this.active = i;
-
-			this.$nextTick(() => {
-				let flkty = Flickity.data(this.$el.querySelectorAll('.carousel')[i]);
-				flkty.resize();
-
-				console.log(flkty);
-			});
-		},
-	};
-}
-
 function myComponentAddToCart() {
 	return {
 		AddToCart(refs) {
